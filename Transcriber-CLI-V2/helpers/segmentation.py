a601c5acce16bd6cbbaeaab16c07f31b7052feae
Riley Herbst <rileyherbst@yahoo.com>
2025-12-04 10:54:50 -0600

    Fixed prompt

--- Transcriber-CLI-V2/helpers/segmentation.py
+++ Transcriber-CLI-V2/helpers/segmentation.py
@@ -324,7 +324,6 @@ class Segmentation:
             "position_original": merged_boxes,
             "position_segmentation": {},
             "base64image_text_segmentation": None,
-        }
 
         # Build crops (NOW with auto-orientation)
         crops_for_segmentation = []
